56c040491fdbc32b750031ededa4feb89b42233b
dengyazhou <1283536241@qq.com>
2022-05-05 10:39:13 +0800

    添加SwiftBase

---
+++ SwiftBase/SwiftBase/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftBase
+//
+//  Created by 邓亚洲 on 2022/5/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("\(#file):\(#line):\(#function)");
+        
+    }
+
+
+}
+
